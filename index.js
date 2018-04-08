0449666a5e26df6a9cd102b0728a7f4f64b54b20
Patrick Gray <graypj826@gmail.com>
2018-04-08 21:06:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,29 +29,9 @@ function dec(a){
 } 
 
 function makeInt(n){
-  if (){
-  return parseInt(a)
+ return Parseint(a,10)
+ 
   }
-  else if(){
-  return 
-  }  
-  else (){
-    
-  }  
-
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
 
 function preserveDecimal(n){
   if (){
