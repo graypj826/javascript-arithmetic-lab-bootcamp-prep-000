45c3cf6b893c4292b349c749541a31ae1d8470ba
Patrick Gray <graypj826@gmail.com>
2018-04-08 21:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,40 +20,22 @@ function divide(a, b){
   return a / b
 } 
 
-function inc(a){
-  return a++
+function inc(n){
+  return n++
 } 
 
-function dec(a){
-  return a--
+function dec(n){
+  return n--
 } 
 
 function makeInt(n){
- return Parseint(a,10)
+ return parseInt(n,10)
  
   }
 
 function preserveDecimal(n){
-  if (){
-    
-  }
-  
-  else (){
-    
-  }
-  
+  return parseFloat(n)
   
 } 
 
 
-
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
